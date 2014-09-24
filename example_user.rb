3e04f4a4315b7ad2acea40f818587db4d7c40749
Draisy <draisy@gmail.com>
2014-09-23 21:29:04 -0400

    Finish layout and routes

---
+++ example_user.rb
@@ -0,0 +1,7 @@
+person1 = {first: "Draisy", last: "Sabel"}
+person2 = {first:"Motty", last: "Brauner"}
+person3 = {first: "Chaya", last: "Brauner"}
+
+params = {father: person1, mother: person2, child:person3}
+
+puts "Father first " + params[:father][:first]
